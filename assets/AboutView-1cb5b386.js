89596f84dee9d671bcf59d60557700b5ef4a94d0
sunwenxu1997 <44797211+sunwenxu1997@users.noreply.github.com>
2025-02-13 10:38:04 +0000

    Deploying to gh-pages from @ sunwenxu1997/chatbot@9998f6d86fb7aae2ee3ef289c4d43bff2021b248 🚀

--- assets/AboutView-1cb5b386.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o,c as s,a}from"./index-36a0d909.js";const n={},_={class:"about"};function c(r,e){return o(),s("div",_,e[0]||(e[0]=[a("h1",null,"This is an about page",-1)]))}const u=t(n,[["render",c]]);export{u as default};
